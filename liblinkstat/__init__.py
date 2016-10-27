0fcb2c60390465f57796205c338f1d6aa5db77c0
François Trahan <francois.trahan@gmail.com>
2016-10-27 17:40:08 -0400

    Importation code initial

---
+++ liblinkstat/__init__.py
@@ -0,0 +1,23 @@
+
+__all__ = [
+    ]
+
+__version__ = "1.0.0-dev"
+
+def inall(obj) :
+    __all__.append(obj.__name__)
+    return obj
+
+@inall
+class Actions :
+    OR = 1,
+    AND = 2,
+    UNIQUE = 3
+
+@inall
+class ReportException(Exception) :
+    def __init__(self, returnValue, *args) :
+        super().__init__(*args)
+        self.returnValue = returnValue
+    
+from .linkstat import LinkStat
